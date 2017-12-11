c4c5f1808494e3ba98b4bd330fc8c725d5d0248b
TomaszCPL <czep.tomasz@gmail.com>
2017-12-11 21:52:00 +0100

    added admin profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,10 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 
 class profile(models.Model):
     name = models.CharField(max_length=120)
+    description = models.TextField(null=True)
+    name = models.CharField(max_length=120)
 
-    def__unicode__(self):
 
