8d2f57d0cfb46dda258fb7ebbc66f1ca6f486a9b
TomaszCPL <czep.tomasz@gmail.com>
2017-12-12 11:31:00 +0100

    added base.html

--- profiles/views.py
+++ profiles/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def home(request):
     context = locals()
     template = 'home.html'
-    return render(request, template)
+    return render(request, template,context)
+
+def about(request):
+    context = locals()
+    template = 'about.html'
+    return render(request, template, context)
